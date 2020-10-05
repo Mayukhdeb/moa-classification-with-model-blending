9365d0fccec81af8d172896d74d82ac8b2aa4bea
mayukhdeb <mayukhmainak2000@gmail.com>
2020-10-05 18:54:27 +0530

    inference: build utils and log_loss

--- src/inference.py
+++ src/inference.py
@@ -5,8 +5,51 @@ from torch.nn import functional as F
 import torch.optim as optim
 from torch.utils.data import DataLoader, Dataset
 
+
+import glob, os
 import optuna
 import numpy as np 
 import pandas as pd 
 from tqdm import tqdm
+from model_classes import *
+from utils import *
 
+
+filenames = [
+    "model_0.pth",
+    "model_1.pth",
+    "model_2.pth",
+    "model_3.pth",
+    "model_4.pth",
+    "model_5.pth",
+    "model_6.pth",
+    "model_100.pth",
+    "model_101.pth",
+    "model_102.pth",
+    "model_103.pth",
+    "model_104.pth",
+    "model_105.pth",
+    "model_106.pth",
+]
+
+model_path = "../models/"
+
+if __name__ == "__main__":
+
+    all_preds = []
+
+    for i in range(len(filenames)):
+        if i < 7:
+            model_class = Model
+            select_features = True
+        else:
+            model_class = Model_2
+            select_features = False
+
+        inference_model = pytorch_model(model_class = model_class, model_path = model_path + filenames[i], device = "cpu")
+        x_train, y_train, x_val, y_val = load_fold(fold = "hold", train_features_path = "../input/lish-moa/train_features.csv", targets_folds_df_path = "../folds/train_targets_folds.csv", select_features= select_features)
+        
+        hold_set = x_val
+        pred = inference_fn(inference_model, test_features = hold_set, device =  "cpu")
+        loss = log_loss_metric(y_true = y_val, y_pred = pred)
+        print("model ", i," log loss: ",  loss)
---
+++ src/model_classes.py
@@ -0,0 +1,63 @@
+import torch.nn as nn
+import torch.nn.functional as F
+
+class Model(nn.Module):
+    def __init__(self):
+        super(Model, self).__init__()
+        self.batch_norm1 = nn.BatchNorm1d(785)
+        self.dropout1 = nn.Dropout(0.2)
+        self.dense1 = nn.utils.weight_norm(nn.Linear(785, 2048))
+        
+        self.batch_norm2 = nn.BatchNorm1d(2048)
+        self.dropout2 = nn.Dropout(0.5)
+        self.dense2 = nn.utils.weight_norm(nn.Linear(2048, 1048))
+        
+        self.batch_norm3 = nn.BatchNorm1d(1048)
+        self.dropout3 = nn.Dropout(0.5)
+        self.dense3 = nn.utils.weight_norm(nn.Linear(1048, 206))
+    
+    def forward(self, x):
+        x = self.batch_norm1(x)
+        x = self.dropout1(x)
+        x = F.relu(self.dense1(x))
+        
+        x = self.batch_norm2(x)
+        x = self.dropout2(x)
+        x = F.relu(self.dense2(x))
+        
+        x = self.batch_norm3(x)
+        x = self.dropout3(x)
+        x = self.dense3(x)
+        
+        return x
+
+
+class Model_2(nn.Module):
+    def __init__(self):
+        super(Model_2, self).__init__()
+        self.batch_norm1 = nn.BatchNorm1d(875)
+        self.dropout1 = nn.Dropout(0.2)
+        self.dense1 = nn.utils.weight_norm(nn.Linear(875, 1048))
+        
+        self.batch_norm2 = nn.BatchNorm1d(1048)
+        self.dropout2 = nn.Dropout(0.5)
+        self.dense2 = nn.utils.weight_norm(nn.Linear(1048, 1048))
+        
+        self.batch_norm3 = nn.BatchNorm1d(1048)
+        self.dropout3 = nn.Dropout(0.5)
+        self.dense3 = nn.utils.weight_norm(nn.Linear(1048, 206))
+    
+    def forward(self, x):
+        x = self.batch_norm1(x)
+        x = self.dropout1(x)
+        x = F.relu(self.dense1(x))
+        
+        x = self.batch_norm2(x)
+        x = self.dropout2(x)
+        x = F.relu(self.dense2(x))
+        
+        x = self.batch_norm3(x)
+        x = self.dropout3(x)
+        x = self.dense3(x)
+        
+        return x
